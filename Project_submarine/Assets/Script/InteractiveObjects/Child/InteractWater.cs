7c94832f6742e84ae87f98074dd9272077e079c6
Mio <arnaudhung@gmail.com>
2024-03-18 17:46:27 +0100

    water fix

--- Project_submarine/Assets/Script/InteractiveObjects/Child/InteractWater.cs
+++ Project_submarine/Assets/Script/InteractiveObjects/Child/InteractWater.cs
@@ -5,23 +5,47 @@ using UnityEngine;
 public class InteractWater : Interactable
 {
     public GameObject water ;
-    private int level_water = 0;
-    private  int level_water_down = 0;
+    private int level_water = 1;
+
+    private void Start()
+    {
+        StartCoroutine(Water_up());
+    }
+
     public override void Interact()
     {
-        if (level_water <= 5)
+        if (level_water == 0) 
         {
-            water.transform.position += new Vector3(0, 0.5f, 0);
-            level_water++;
+            return;
         }
         else
         {
-            water.transform.position -= new Vector3(0, 0.5f, 0);
-            level_water_down++;
-                if (level_water_down == 5)
-            {
-                level_water = 0;
-            }
+            water.transform.position -= new Vector3(0, 0.1f, 0);
+            level_water--;
+            level_water = Mathf.Clamp(level_water, 0, 6);
+            Debug.Log(level_water);
         }
+    }
+    private IEnumerator Water_up()
+    {
+        while (true) 
+        {
+            level_water = Mathf.Clamp(level_water, 0, 6);
+            if (level_water <= 5)
+            {
+
+                water.transform.position += new Vector3(0, 0.1f, 0);
+                level_water++;
+                Debug.Log(level_water);
+            }
+
+            if (level_water <= 0)
+            {
+                yield return new WaitForSecondsRealtime(2);
+            }
+
+            yield return new WaitForSecondsRealtime(2);
+        }
+
     }
 }
