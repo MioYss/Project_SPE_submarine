b89d42368669efc83aa5da40476fbd106b267682
MioYss <arnaudhung@gmail.com>
2024-04-10 09:51:41 +0200

    InteractMouse ok

---
+++ Project_submarine/Assets/Script/InteractiveObjects/Child/InteractMouse.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractMouse : Interactable
+{
+    public GameObject fps_controller;
+    public GameObject fps_controller02;
+    public override void Interact()
+    {
+        fps_controller.GetComponent<FPSCameraController>().enabled = false;
+        fps_controller.GetComponent<Mouvement>().enabled = false;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void Update()
+    {
+        CancelMouse();
+    }
+    private void CancelMouse()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            fps_controller.GetComponent<FPSCameraController>().enabled = true;
+            fps_controller.GetComponent<Mouvement>().enabled = true;
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+}
