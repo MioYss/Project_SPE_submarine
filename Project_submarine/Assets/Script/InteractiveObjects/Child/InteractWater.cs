14c2a7ea7b422c6c1f9820b96e37debe86c85db2
MioYss <arnaudhung@gmail.com>
2024-04-15 18:40:31 +0200

    eau ok

--- Project_submarine/Assets/Script/InteractiveObjects/Child/InteractWater.cs
+++ Project_submarine/Assets/Script/InteractiveObjects/Child/InteractWater.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,8 +7,26 @@ public class InteractWater : Interactable
     public GameObject water ;
     private int level_water = 1;
 
+    public bool LancerEau;
+    public bool EnleverEau;
+
     private void Start()
     {
+
+    }
+
+    private void Update()
+    {
+        if(LancerEau == true)
+        {
+            StartCoroutine(Water_Up());
+            LancerEau = false;
+        }
+        if(EnleverEau == true)
+        {
+            StartCoroutine(Water_Down());
+            EnleverEau = false;
+        }
     }
 
     public void Put_Water_Down()
@@ -45,9 +63,9 @@ public class InteractWater : Interactable
                 //Debug.Log(level_water);
             }
 
-            if (level_water <= 0)
+            if (level_water >= 0)
             {
-                yield return new WaitForSecondsRealtime(2);
+                yield return new WaitForSecondsRealtime(5);
             }
 
             yield return new WaitForSecondsRealtime(2);
@@ -57,7 +75,7 @@ public class InteractWater : Interactable
 
     private IEnumerator Water_Down()
     {
-        var newpos = water.transform.position + new Vector3(0, -10f, 0);
+        var newpos = water.transform.position + new Vector3(0, -1f, 0);
         while ((newpos - water.transform.position).magnitude > 0.01f)
         {
             water.transform.position = Vector3.Slerp(water.transform.position, newpos, 0.01f);
